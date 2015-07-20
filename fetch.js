8822cea5f23c2d7dda6ca0ed5b83956b6e0c25f1
Richard Mavis <richard@airtype.com>
2015-07-20 15:51:48 -0700

    Seems to work. See documentation in the script. Added a HTML page for console testing.

--- fetch.js
+++ fetch.js
@@ -1,55 +1,134 @@
-function Fetch(params) {
+/* FETCH
+ *
+ * This is an object for making HTTP requests.
+ *
+ * The public methods indicate the HTTP verb that will be used
+ * when they are called. They can receive the same parameter,
+ * which is an object that should have the pertinent key-value
+ * pairs as represented by the conf variable, less the `method`
+ * key, which will be filled by the method.
+ *
+ *
+ * EXAMPLES
+ *
+ * This:
+ * Fetch.get({ url: "http://example.com",
+ *             params: {foo:"bar", boo:"bat"},
+ *             callback: handler });
+ * will generate the request:
+ * GET http://example.com?foo=bar&boo=bat
+ * and pass the response to function `handler`.
+ */
 
-    this.init = function(pobj) {
-        this.url = ('url' in pobj) ? pobj.url : null;
-        this.callback = ('callback' in pobj) ? pobj.callback : null;
+var Fetch = (function () {
 
-        if (this.url && this.callback) {
-            this.getIt();
-        }
+    var conf = {
+        url: null,
+        method: null,
+        params: null,
+        callback: null
     };
 
 
 
-    this.getIt = function() {
-        console.log("GETting " + this.url);
-        // return;
+    function init(obj) {
+        for (var key in obj) {
+            if ((obj.hasOwnProperty(key)) &&
+                (conf.hasOwnProperty(key))) {
+                conf[key] = obj[key];
+            }
+        }
+    }
 
+
+
+    function reset() {
+        for (var key in conf) {
+            if (conf.hasOwnProperty(key)) {
+                conf[key] = null;
+            }
+        }
+    }
+
+
+
+    function http() {
         var xhr = (window.XMLHttpRequest)
             ? (new XMLHttpRequest())
             : (new ActiveXObject("Microsoft.XMLHTTP"));
-        xhr.open('GET', this.url);
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        xhr.send();
 
-        var handler = this.handleTheReturn.bind(this);
+        if (conf.method == 'get') {
+            if (conf.params) {
+                // console.log('GETting ' + conf.url + '?' + toParamString(conf.params));
+                xhr.open(conf.method, conf.url + '?' + toParamString(conf.params));
+                xhr.send();
+            } else {
+                // console.log('GETting ' + conf.url);
+                xhr.open(conf.method, conf.url);
+                xhr.send();
+            }
+        }
+
+        else if (conf.method == 'post') {
+            xhr.open(conf.method, conf.url);
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            if (conf.params) {
+                // console.log('POSTting ' + toParamString(conf.params) + ' to ' + conf.url);
+                xhr.send(conf.params);
+            } else {
+                // console.log('POSTting nothing to ' + conf.url);
+                xhr.send();
+            }
+        }
+
+        else {
+            //             console.log("Unsupported HTTP verb: " + conf.method);
+        }
+
+        var handler = wrapup.bind(this);
 
         xhr.onreadystatechange = function() {
             if ((xhr.readyState == 4) && (xhr.status == 200)) {
                 handler(xhr.responseText);
             }
         };
+    }
+
+
+
+    function wrapup(response) {
+        if (conf.callback) {
+            // console.log("Sending " + response + " to " + conf.callback);
+            conf.callback(response);
+        }
+        else {
+            // console.log("No callback");
+        }
+
+        reset();
+    }
+
+
+
+
+
+    /*
+     * Public methods.
+     */
+
+    return {
+        get: function(obj) {
+            init(obj);
+            conf.method = 'get';
+            http();
+        },
+
+
+        post: function(obj) {
+            init(obj);
+            conf.method = 'post';
+            http();
+        }
     };
-
-
-
-    this.handleTheReturn = function(response) {
-        // No need to check for the handler.
-        // That happens in init.
-        this.callback(response);
-        this.reset();
-    };
-
-
-
-    this.reset = function() {
-        this.url = null;
-        this.callback = null;
-    };
-
-
-
-    // This needs to stay down here.
-    if (typeof params == 'object') {this.init(params);}
-    else {this.init({});}
-}
+})();
